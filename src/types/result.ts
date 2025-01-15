c4ced6c132da7b7468ee396200ad06131dd26c05
Abhinav Chauhan <abhinav.chauhan.code@gmail.com>
2025-01-15 11:24:49 +0530

    Update result.ts

--- src/types/result.ts
+++ src/types/result.ts
@@ -49,3 +49,43 @@ export interface StudentResult {
     sessionto: Date;
   };
 }
+
+export interface StudentResult11 {
+  student: {
+    name: string;
+    birthday: Date;
+    Class: { name: string };
+    Section: { name: string };
+    admissionno: number;
+    mothername: string;
+    fathername: string;
+    address: string;
+    city: string;
+    village: string;
+    img?: string;
+  };
+  marksHigher: Array<{
+    unitTest1: number | null;
+    halfYearly: number | null;
+    unitTest2: number | null;
+    theory: number | null;
+    practical: number | null;
+    totalWithout: number | null;
+    grandTotal: number | null;
+    total: number | null;
+    percentage: number | null;
+    grade: string | null;
+    remarks: string | null;
+    sectionSubject: {
+      subject: {
+        name: string;
+        code: string;
+      };
+    };
+  }>;
+  session: {
+    sessioncode: string;
+    sessionfrom: Date;
+    sessionto: Date;
+  };
+}
